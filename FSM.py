824de598c82cbd25546fd17d5bc8882ed1415ac0
Hanna Torjusen <hanntorj@stud.ntnu.no>
2019-10-05 14:29:26 +0200

    setup - classes

---
+++ FSM.py
@@ -0,0 +1,6 @@
+
+class FSM:
+        '''Holds a pointer back to the agent, since it will make many requests to the KCP'''
+
+        def add_rule:
+            '''add a new rule to the end of the FSM's rule list'''
