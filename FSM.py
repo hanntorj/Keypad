a527e8e1b9e9db9b7b36db8359c08e6b4e69db8e
Hanna Torjusen <hanntorj@stud.ntnu.no>
2019-10-05 14:39:28 +0200

    test branch other

--- FSM.py
+++ FSM.py
@@ -4,3 +4,13 @@ class FSM:
 
         def add_rule:
             '''add a new rule to the end of the FSM's rule list'''
+        def get_next_signal:
+            '''query the agent for the next signal'''
+        def run_rules:
+            '''go through the rule set, in order, applying each rule until one of the rules is fired'''
+        def apply_rule:
+            '''check whether the conditions of a ruøe are met'''
+        def fire_rule:
+            '''use the consequent of a rule to a) set the next state to FSM and b) call the appropriate agent action method'''
+        def main_loop:
+            '''begin in the FSM's default initial state and then repeatedly call get_next_signal and run_rules until the FSM enters its default final stat'''
